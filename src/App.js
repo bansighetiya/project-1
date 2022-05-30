8d9b445fc9a81c49122f4f7c0a03fdb48a3723f3
bansighetiya <bansighetiya7@gmail.com>
2022-05-30 15:58:12 +0530

    30_5_b

--- src/App.js
+++ src/App.js
@@ -4,25 +4,25 @@ import Demo from './component/Demo';
 
 function App() {
 
-  const person = {
-    name: "Amit",
-    age: 25
-  }
+  // const person = {
+  //   name: "Amit",
+  //   age: 25
+  // }
 
-  console.log(person.name, person.age);
+  // console.log(person.name, person.age);
 
-  // const person = [
-  //   {
-  //     name: "Amit",
-  //     age: 25,
-  //   },
-  //   {
-  //     name: "Piyush",
-  //     age: 40,
-  //   },
-  // ];
+  const person = [
+    {
+      name: "Amit",
+      age: 25,
+    },
+    {
+      name: "Piyush",
+      age: 40,
+    },
+  ];
 
-  // person.map((p) => console.log(p.name, p.age));
+  person.map((p) => console.log(p.name, p.age));
 
   // const person = {
   //   name: "Amit",
